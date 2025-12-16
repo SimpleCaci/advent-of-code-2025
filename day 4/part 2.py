41ae1cfdf2d8a6552f94bf5d1f9762db32279a3d
SimpleCaci <edikus@outlook.es>
2025-12-15 17:34:03 -0800

    d4pt2

--- day 4/part 2.py
+++ day 4/part 2.py
@@ -2,17 +2,19 @@
 
 file_name = r'C:\Users\Ediku\Escritorio\MISC\CS Programming\Personal Projects\Github\SimpleCaci\advent-of-code-2025\day 4\input.txt'
 
-with open(file_name) as f:
-    grid = [list(line.strip()) for line in f]
 
-rowSize = len(grid)
-colSize = len(grid[0])
+
+with open(file_name) as f:
+    input = [list(line.strip()) for line in f]
+
+rowSize = len(input)
+colSize = len(input[0])
 
 # Padding
-newGrid = [['.' for _ in range(colSize+2)] for _ in range(rowSize+2)]
+currGrid = [['.' for _ in range(colSize+2)] for _ in range(rowSize+2)]
 for r in range(rowSize):
     for c in range(colSize):
-        newGrid[r+1][c+1] = grid[r][c]
+        currGrid[r+1][c+1] = input[r][c]
 
 
 def countOccupiedRoll(grid, row, col):
@@ -33,12 +35,24 @@ def countOccupiedRoll(grid, row, col):
 
 
 moveable = 0
-
+previousMoveable = -1  # to ensure the loop starts
+newGrid = currGrid.copy() 
 # must iterate in padded coordinates
-for r in range(1, rowSize+1):
-    for c in range(1, colSize+1):
-        if newGrid[r][c] == '@':  # only rolls matter
-            if countOccupiedRoll(newGrid, r, c) < 4:
-                moveable += 1
+while moveable != previousMoveable:
+    previousMoveable = moveable
+    for r in range(1, rowSize+1):
+        for c in range(1, colSize+1):
+            if currGrid[r][c] == '@':  # only rolls matter
+                if countOccupiedRoll(currGrid, r, c) < 4:
+                    moveable += 1
+                    newGrid[r][c] = '.'  # mark as moved
+    currGrid = newGrid.copy()  # update the grid for the next iteration
+    print(currGrid)
+    print(newGrid)
+
+        
+    
+
+                
 
 print("Total Moveable Paper:", moveable)
